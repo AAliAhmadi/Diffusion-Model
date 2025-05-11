e9e0212c750075189104e705903d06fc0cc0ca2f
AAliAhmadi <93519287+AAliAhmadi@users.noreply.github.com>
2025-05-12 00:16:52 +0330

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,16 +1,16 @@
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
-from config import IMG_SIZE, DATA_PATH, BATCH_SIZE
+from config import Config  # Import the Config class
 
 def get_transforms():
     return transforms.Compose([
-        transforms.Resize((IMG_SIZE, IMG_SIZE)),
+        transforms.Resize((Config.image_size, Config.image_size)),  # Use Config class here
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Lambda(lambda t: (t * 2) - 1)
+        transforms.Lambda(lambda t: (t * 2) - 1)  # Normalize the image to [-1, 1]
     ])
 
 def get_dataloader():
-    dataset = ImageFolder(root=DATA_PATH, transform=get_transforms())
-    return DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+    dataset = ImageFolder(root=Config.data_dir, transform=get_transforms())  # Use Config class here
+    return DataLoader(dataset, batch_size=Config.batch_size, shuffle=True, drop_last=True)  # Use Config class here
